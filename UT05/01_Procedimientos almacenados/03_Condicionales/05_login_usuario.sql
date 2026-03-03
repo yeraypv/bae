27542d7fc0fb16f9fe1ce4a3ee965c043d28fee9
yeraypv <158301868+yeraypv@users.noreply.github.com>
2026-03-03 19:48:48 +0000

    Update 05_login_usuario.sql
    Actualización del login de usuario

--- UT05/01_Procedimientos almacenados/03_Condicionales/05_login_usuario.sql
+++ UT05/01_Procedimientos almacenados/03_Condicionales/05_login_usuario.sql
@@ -19,39 +19,58 @@ BEGIN
     DECLARE _intentos INT;
     DECLARE _bloqueado BOOLEAN;
     DECLARE _activo BOOLEAN;
+    DECLARE _existe INT DEFAULT 0;
 
-    -- Obtener datos del usuario
-    SELECT clave, intentos_fallidos, bloqueado, activo
-    INTO _clave, _intentos, _bloqueado, _activo
+    -- Verificamos que el usuario existe
+    SELECT COUNT(*) INTO _existe
     FROM usuarios
     WHERE usuario = p_usuario;
 
-    -- Verificar que exista y esté activo
-    IF _activo = TRUE AND _bloqueado = FALSE THEN
-
-        -- Verificar contraseña
-        IF _clave = p_clave THEN
-            
-            UPDATE usuarios
-            SET intentos_fallidos = 0
-            WHERE usuario = p_usuario;
-
-            SELECT 'Login correcto' AS mensaje;
-
-        ELSE
-            
-            UPDATE usuarios
-            SET intentos_fallidos = intentos_fallidos + 1
-            WHERE usuario = p_usuario;
-
-            SELECT 'Password incorrecto' AS mensaje;
-
-        END IF;
-
+    IF _existe = 0 THEN
+        SELECT 'Credenciales incorrectas' AS mensaje;
     ELSE
-        SELECT 'Usuario bloqueado o inactivo' AS mensaje;
-    END IF;
+        -- Obtenemos los datos del usuario que existe.
+        SELECT clave, intentos_fallidos, bloqueado, activo
+        INTO _clave, _intentos, _bloqueado, _activo
+        FROM usuarios
+        WHERE usuario = p_usuario;
+        
+        -- Verificamos el estado y si esta inactivo.
+        IF _bloqueado = TRUE THEN
+            SELECT 'Usuario bloqueado' AS mensaje;
+        ELSE
+            IF _activo = FALSE THEN
+                SELECT 'Usuario inactivo' AS mensaje;
+            ELSE
+                -- Número de intentos permitidos son 3 como máximo, 
+                -- si se superan debe bloquearse el usuario.
+                IF _intentos >= 3 THEN 
+                    -- Se han superado el número de intentos, se bloque el usuario.
+                    UPDATE usuarios
+                    SET bloqueado = TRUE
+                    WHERE usuario = p_usuario;
 
+                    SELECT 'Usuario bloqueado por superar 3 intentos' AS mensaje;
+                ELSE
+                    -- Verificamos la contraseña
+                    IF _clave = p_clave THEN
+                        -- Al tener éxito en el login resetamos la cuenta de los intentos.
+                        UPDATE usuarios
+                        SET intentos_fallidos = 0
+                        WHERE usuario = p_usuario;
+
+                        SELECT 'Login correcto' AS mensaje;
+                    ELSE
+                        UPDATE usuarios
+                        SET intentos_fallidos = intentos_fallidos + 1
+                        WHERE usuario = p_usuario;
+
+                        SELECT 'Password incorrecto' AS mensaje;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
+    END IF;
 END;$$
 
 DELIMITER ;
